4ad9cfc11486421552d90e2c84d93879742710bc
Hemsundar Paranthaman <hemsundar.paranthaman@outlook.com>
2023-01-25 08:05:36 +0530

    add about section styles

--- ui/Common/TitleText.tsx
+++ ui/Common/TitleText.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 
 const TitleText: FC<{ text: string; styles?: string }> = ({ text, styles }) => {
   return (
-    <h1 className={`text-6xl md:text-8xl font-medium ${styles}`}>{text}</h1>
+    <h1 className={`text-6xl lg:text-7xl xl:text-8xl font-medium ${styles}`}>
+      {text}
+    </h1>
   );
 };
 
--- ui/HomePage/AboutSection.tsx
+++ ui/HomePage/AboutSection.tsx
@@ -2,31 +2,46 @@ import { FC } from "react";
 import Image from "next/image";
 
 const AboutSection: FC = () => {
-  return (
-    <section className="px-4 py-10 lg:px-20 lg:py-20">
-      {/* <h1>About me</h1> */}
-      <p className="text-3xl md:text-4xl lg:text-5xl leading-[40px] lg:leading-[60px]">
-        Hello, I&apos;m Hemsundar, a freelancer based in India. I specialize in
-        designing and developing Front-end web experiences. I am very passionate
-        about improving web design and continuously learning new things to
-        ensure I offer you the best.
-      </p>
-      <h2 className="text-3xl md:text-4xl lg:text-5xl mt-10">
-        Technologies I use:
-        <div>
-          <div>
-            <div className="relative aspect-square h-28">
-              <Image
-                src={"/1174949_js_react js_logo_react_react native_icon-2.png"}
-                alt={"React Logo"}
-                fill
-                objectFit={"cover"}
-              />
-            </div>
-            <span>React</span>
-          </div>
+  const IconCard: FC<{ imageUrl: string; name: string }> = ({
+    imageUrl,
+    name,
+  }) => {
+    return (
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className="relative aspect-square w-16">
+          <Image src={imageUrl} alt={"Logo"} fill objectFit="cover" />
         </div>
-      </h2>
+        <span className="text-xl">{name}</span>
+      </div>
+    );
+  };
+
+  return (
+    <section className="px-4 py-10 lg:px-20 lg:py-20 flex gap-10 flex-col md:flex-row md:items-start">
+      <div className="md:basis-2/6 w-full relative max-w-xl aspect-square ">
+        <Image
+          src={"/my-photo.jpg"}
+          alt={"My Photo"}
+          objectFit="contain"
+          fill
+        />
+      </div>
+      <div className="md:basis-4/6 w-full">
+        <p className="text-3xl md:text-3xl lg:text-4xl leading-[40px] md:leading-[50px] lg:leading-[60px]">
+          Hello, I&apos;m Hemsundar, a freelancer based in India. I specialize
+          in designing and developing Front-end web experiences. I am very
+          passionate about improving web design and continuously learning new
+          things to ensure I offer you the best.
+        </p>
+        <h2 className="text-3xl md:text-4xl mt-10">Technologies I use:</h2>
+        <div className="flex">
+          <IconCard imageUrl={"/react-logo.png"} name={"React"} />
+          <IconCard
+            imageUrl={"/9118036_nextjs_fill_icon.png"}
+            name={"Nextjs"}
+          />
+        </div>
+      </div>
     </section>
   );
 };
